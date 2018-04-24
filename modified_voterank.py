2dc1fe62f321e6aa886112551ecde38d5b34f3cc
priyankarathnam <pcr43@cornell.edu>
2018-04-24 11:24:40 -0400

    added information entropy and simulated dataset

--- modified_voterank.py
+++ modified_voterank.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import random
+import math
 
 nodes_set = set()
 edges_set = set()
@@ -8,6 +9,7 @@ node_neighbors_dict = {}
 num_initial_spreaders = 2
 elected_spreaders = set()
 longest_shortest_path = 14
+entropy_parameter = .5
 
 with open('simulated.txt') as inputfile:
 	for line in inputfile:
@@ -23,13 +25,31 @@ neighbors_dict = defaultdict(set)
 for (node_0, node_1) in edges_set:
 	neighbors_dict[node_0].add(node_1)
 	neighbors_dict[node_1].add(node_0)
-
+print(neighbors_dict)
 # average degree of network
 average_degree = 0
 for node, neighbors in neighbors_dict.items():
 	average_degree += len(neighbors)
 average_degree /= num_nodes
 print("average degree of the graph is :" + str(average_degree))
+
+def get_pj(node, target_node, neighbors_dict):
+	return len(neighbors_dict[node])/sum([len(neighbors_dict[n]) for n in neighbors_dict[target_node]])
+
+def get_entropy(target_node, neighbors_dict):
+	numerator = sum([-get_pj(neighbor, target_node, neighbors_dict)*math.log(get_pj(neighbor, target_node, neighbors_dict)) for neighbor in neighbors_dict[target_node]])
+	denominator = math.log(len(neighbors_dict[target_node]))
+	if denominator == 0:
+		denominator = 1
+	#print(target_node)
+	#print(numerator/denominator)
+	return numerator/denominator
+
+def votes_with_entropy(num_votes_received, node, neighbors_dict, scale = True):
+	if scale:
+		return num_votes_received*get_entropy(node, neighbors_dict)
+	else:
+		return num_votes_received+entropy_parameter*get_entropy(node, neighbors_dict)*num_votes_received
 
 def vote_and_elect(node_voting_info):
 	node_with_max_votes = ""
@@ -39,6 +59,7 @@ def vote_and_elect(node_voting_info):
 		num_votes_received = 0
 		for neighbor in neighbors:
 			num_votes_received += node_voting_info[neighbor][1]
+		num_votes_received = votes_with_entropy(num_votes_received, node, neighbors_dict, True)
 		if num_votes_received > max_votes:
 			max_votes = num_votes_received
 			node_with_max_votes = node
